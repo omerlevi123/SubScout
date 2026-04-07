b653be98b7e35ed98237c220310e603ecc4e24c0
Omer Levi <leviomer200@gmail.com>
2026-04-07 21:52:54 +0300

    "Update API baseURL for Render deployment"

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,14 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-})
+  baseURL: `${import.meta.env.VITE_API_URL || "http://localhost:5000"}/api`,
+});
 
 export const getSubscriptions = (userId) =>
-  api.get(`/subscriptions?user_id=${userId}`)
+  api.get(`/subscriptions?user_id=${userId}`);
 
-export const createSubscription = (data) =>
-  api.post('/subscriptions', data)
+export const createSubscription = (data) => api.post("/subscriptions", data);
 
 export const addRating = (id, rating) =>
-  api.post(`/subscriptions/${id}/rating`, { rating })
+  api.post(`/subscriptions/${id}/rating`, { rating });
